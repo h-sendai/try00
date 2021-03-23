ffe0dbbebab964dfacf40261c45cbcc947cecca3
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2021-03-23 12:28:58 +0900

    add stub C program files

---
+++ sample.c
@@ -0,0 +1,17 @@
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
